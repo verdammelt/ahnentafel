37a6249a70ac3e07568852c377266b9101255681
Mark Simpson <verdammelt@gmail.com>
2015-01-19 16:43:21 -0500

    SQUASH Adding some build tools.
    * lein-ancient to check for updates
    * lein-bikeshed for linting
    * lein-kibit for style checking.

--- project.clj
+++ project.clj
@@ -5,12 +5,17 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
 
-  :profiles {:dev {:resource-paths ["test-resources"]
-                   :dependencies [[lein-ancient "0.6.0"]
-                                  [lein-bikeshed "0.2.0"]
-                                  [lein-kibit "0.0.8"]]
-                   :aliases {"check-update" ["ancient" ":all"]
-                             "lint" ["do" ["bikeshed" "-v"] ["kibit"]]
-                             "build" ["do" "clean" ["test"] ["lint"] ]}}}
+  :profiles
+  {:dev
+   {:resource-paths ["test-resources"]
+    :dependencies [[lein-ancient "0.6.0"]
+                   [lein-bikeshed "0.2.0"]
+                   [lein-kibit "0.0.8"]]
+    :aliases {"check-update" ^{:doc "Check for upgrades to dependencies and plugins."}
+              ["ancient" ":all"]
+              "lint" ^{:doc "Check code for linting and style errors."}
+              ["do" ["bikeshed" "-v"] ["kibit"]]
+              "build" ^{:doc "Full clean build with tests and linting."}
+              ["do" "clean" ["test"] ["lint"] ]}}}
 
   :aot [ahnentafel.gedcom.ParseError])
