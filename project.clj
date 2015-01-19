db1a00d749187c48a21b98abaf6254ad4a2a5b24
Mark Simpson <verdammelt@gmail.com>
2015-01-19 15:37:55 -0500

    Adding build target.

--- project.clj
+++ project.clj
@@ -4,5 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:resource-paths ["test-resources"]}}
+
+  :profiles {:dev {:resource-paths ["test-resources"]
+                   :aliases {"build" ["do" "clean" ["test"]]}}}
+
   :aot [ahnentafel.gedcom.ParseError])
