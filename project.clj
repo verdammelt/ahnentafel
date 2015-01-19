ee091d01271a3e6b871b395a336766cba6d78e7e
Mark Simpson <verdammelt@gmail.com>
2015-01-19 15:50:38 -0500

    Adding some build tools.
    * lein-ancient to check for updates
    * lein-bikeshed for linting
    * lein-kibit for style checking.

--- project.clj
+++ project.clj
@@ -6,6 +6,11 @@
   :dependencies [[org.clojure/clojure "1.6.0"]]
 
   :profiles {:dev {:resource-paths ["test-resources"]
-                   :aliases {"build" ["do" "clean" ["test"]]}}}
+                   :dependencies [[lein-ancient "0.6.0"]
+                                  [lein-bikeshed "0.2.0"]
+                                  [lein-kibit "0.0.8"]]
+                   :aliases {"check-update" ["ancient" ":all"]
+                             "lint" ["do" ["bikeshed" "-v"] ["kibit"]]
+                             "build" ["do" "clean" ["test"] ["lint"] ]}}}
 
   :aot [ahnentafel.gedcom.ParseError])
