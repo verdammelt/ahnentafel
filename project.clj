ff1b7191ad97ca4f90246e1d154cc47624d6ccd2
Mark Simpson <verdammelt@gmail.com>
2015-01-19 20:22:26 -0500

    With lein-ring I don't need to include ring explicitly.

--- project.clj
+++ project.clj
@@ -3,9 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring-core "1.3.2"]
-                 [ring/ring-jetty-adapter "1.3.2"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
 
   :plugins [[lein-ring "0.9.1"]]
 
