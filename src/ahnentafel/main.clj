beb3d287c21b0998e95824f82a155c14e7dfdac0
Mark Simpson <verdammelt@gmail.com>
2015-02-16 16:15:06 -0500

    Moving pages code.

--- src/ahnentafel/main.clj
+++ src/ahnentafel/main.clj
@@ -1,19 +1,14 @@
 (ns ahnentafel.main
+  (:require [ahnentafel.pages.pages :as pages])
   (:require [compojure.core :refer :all])
   (:require [ring.util.response :as response])
   (:require [ring.middleware.defaults :refer [wrap-defaults site-defaults]])
-  (:require [net.cgrand.enlive-html :as html]))
-
-;; move these views into a new mainspace (to encapsulate enlive)
-(html/defsnippet home-page "site/templates/home.html" [:div] [])
-(html/deftemplate main-template "site/templates/index.html" [contents data]
-  [:#content] (html/substitute (contents))
-  [:#version] (html/content (:version data)))
+  )
 
 (def app-context {:version (System/getProperty "ahnentafel.version")})
 
 (defroutes main-handler
-  (GET "/" [] (main-template home-page app-context))
+  (GET "/" [] (pages/home-page app-context))
   (ANY "*" request (->  (str (:uri request) " not found.")
                         response/response
                         (response/status 404)
---
+++ src/ahnentafel/pages/pages.clj
@@ -0,0 +1,8 @@
+(ns ahnentafel.pages.pages
+  (:require [net.cgrand.enlive-html :as html]))
+
+(html/defsnippet home-page-snippet "site/templates/home.html" [:div] [])
+
+(html/deftemplate home-page "site/templates/index.html" [data]
+  [:#content] (html/substitute (home-page-snippet))
+  [:#version] (html/content (:version data)))
