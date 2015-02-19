f9f6e4ba7950ce4f47c4376ebe03d042feabeea2
Mark Simpson <verdammelt@gmail.com>
2015-02-18 21:47:48 -0500

    Beginning to implement the "reloaded" workflow.
    Totally stolen from Stuart Sierra's blog post
    http://thinkrelevance.com/blog/2013/06/04/clojure-workflow-reloaded

--- project.clj
+++ project.clj
@@ -10,15 +10,18 @@
   :plugins [[lein-ring "0.9.1"]]
 
   :aot [ahnentafel.gedcom.ParseError]
-  :ring {:handler ahnentafel.main/app}
+  :ring {:handler ahnentafel.main/app
+         :auto-refresh? true}
 
   :profiles
   {:dev
-   {:resource-paths ["test-resources"]
+   {:source-paths ["dev"]
+    :resource-paths ["test-resources"]
     :plugins [[lein-ancient "0.6.2"]
               [lein-bikeshed "0.2.0"]
               [lein-kibit "0.0.8"]]
-    :dependencies [[ring/ring-mock "0.2.0"]]
+    :dependencies [[ring/ring-mock "0.2.0"]
+                   [ring/ring-jetty-adapter "1.3.1"]]
     :aliases {"check-update" ^{:doc "Check for upgrades to dependencies and plugins."}
               ["ancient" ":all"]
               "lint" ^{:doc "Check code for linting and style errors."}
