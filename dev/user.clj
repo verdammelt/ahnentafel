85c2723c857fe236d32a40f1da1dee53a2ee6118
Mark Simpson <verdammelt@gmail.com>
2015-02-22 19:45:09 -0500

    Even more "reloaded" style refactoring.
    Now with system/start and system/stop methods - currently no-ops.

--- dev/user.clj
+++ dev/user.clj
@@ -19,26 +19,44 @@
   nil)
 
 (defn- stop-server [system]
-  (when-let [server (:server system)] (.stop server)))
+  (when-let [server (:server system)]
+    (.stop server)
+    (dissoc system :server)))
 
 (defn- start-server [system port]
   (let [server (run-jetty (:handler system) {:port port :join? false})]
     (assoc system :server server)))
 
+(defn init []
+  "Construct the current environment"
+  (alter-var-root #'system (constantly (system/system))))
+
 (defn stop []
   "Shut down the system, if running."
   (println "Shutting down")
-  (alter-var-root #'system (fn [s] (when s (stop-server s) nil))))
+  (alter-var-root #'system
+                  (fn [s] (when s
+                           (-> s
+                               stop-server
+                               system/stop)))))
 
 (defn start
   "Start up the system."
   ([] (start 3000))
   ([port]
    (println "Starting up on port" port)
-   (alter-var-root #'system (fn [_] (start-server (system/system) port)))))
+   (alter-var-root #'system
+                   (fn [s] (-> s
+                              system/start
+                              (start-server port))))))
+
+(defn go []
+  "Initialize the current development system and start it running."
+  (init)
+  (start))
 
 (defn reset []
   "Stop the running system (if any), refresh namespaces and start the
 system up."
   (stop)
-  (refresh :after 'user/start))
+  (refresh :after 'user/go))
