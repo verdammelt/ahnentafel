c3ca7770632872e945298dc4205eff3f72587de1
Mark Simpson <verdammelt@gmail.com>
2015-02-22 19:27:40 -0500

    Moving code around - now there is a server namespace.
    All things related to the functioning of the web server is now in
    ahnentafel.server.

--- src/ahnentafel/main.clj
+++ src/ahnentafel/main.clj
@@ -1,22 +1,6 @@
 (ns ahnentafel.main
-  (:require [ahnentafel.pages.pages :as pages])
-  (:require [ahnentafel.middleware.logging :refer [simple-logging]])
-
-  (:require [environ.core :refer [env]])
-  (:require [compojure.core :refer :all])
-  (:require [ring.util.response :as response])
-  (:require [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
-
-(defn make-handler [app-data]
-  (simple-logging
-   (wrap-defaults
-    (routes
-     (GET "/" [] (pages/home-page app-data))
-     (ANY "*" request (-> (pages/page-not-found (merge request app-data))
-                          response/response
-                          (response/status 404)
-                          (response/header "Content-Type" "text/html"))))
-    (assoc-in site-defaults [:static :resources] "site"))))
+  (:require [ahnentafel.server.handler :refer [make-handler]])
+  (:require [environ.core :refer [env]]))
 
 (defn system []
   (let [app-data {:version (env :ahnentafel-version)}
