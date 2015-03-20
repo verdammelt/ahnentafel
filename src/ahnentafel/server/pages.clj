97f811dc8f5267729833a4006b4c3e8b9f113f3a
Mark Simpson <verdammelt@gmail.com>
2015-03-20 14:24:31 -0400

    Adding a tiny bit of templating.

--- src/ahnentafel/server/pages.clj
+++ src/ahnentafel/server/pages.clj
@@ -1,10 +1,16 @@
 (ns ahnentafel.server.pages
   (:require [net.cgrand.enlive-html :as html]))
 
-(html/defsnippet home-page-snippet "site/templates/home.html" [:div] [])
+(html/defsnippet home-page-snippet "site/templates/home.html" [:div] [data]
+  (let [file (:file data)
+        num-records (count ((:get-data data)))]
+    [:#home-contents]
+    (html/transform-content
+     (html/replace-vars {:num-records (str num-records) :file (str file)}))))
+
 
 (html/deftemplate home-page "site/templates/index.html" [data]
-  [:#content] (html/substitute (home-page-snippet))
+  [:#content] (html/substitute (home-page-snippet data))
   [:#version] (html/content (:version data)))
 
 (html/deftemplate page-not-found "site/templates/index.html" [data]
--- test/ahnentafel/server/pages_test.clj
+++ test/ahnentafel/server/pages_test.clj
@@ -3,10 +3,13 @@
   (:require [clojure.test :refer :all]))
 
 (deftest home-page-test
-  (let [page (apply str (home-page {:version "x.x.x"}))]
+  (let [page (apply str (home-page {:version "x.x.x"
+                                    :get-data (fn [] (list 1 2 3))
+                                    :file "test.ged"}))]
     (is (.contains page "<title>Ahnentafel</title>"))
     (is (.contains page "id=\"home-contents\""))
-    (is (.contains page "<span id=\"version\">x.x.x</span>"))))
+    (is (.contains page "<span id=\"version\">x.x.x</span>"))
+    (is (.contains page "3 records in test.ged."))))
 
 (deftest page-not-found-test
   (let [page (apply str (page-not-found {:uri "/unknown" :version "x.x.x"}))]
