0e2cc5618915d2a18669b29d4d2dec728849f662
Mark Simpson <verdammelt@gmail.com>
2015-04-26 13:09:24 -0400

    Tests needed changing after last change.
    Now reading a GEDCOM file results in a  structure with a 'root' node.
    The tests still expected the old structure.

--- test/ahnentafel/gedcom/reading/reader_test.clj
+++ test/ahnentafel/gedcom/reading/reader_test.clj
@@ -4,20 +4,22 @@
 
 (deftest reader
   (testing "usual behavior"
-    (let [records (read-file (clojure.java.io/resource "simple.ged"))]
-      (is (= 7 (count records)))
-
-      (is (= "HEAD" (:tag (first records))))
-      (is (= "CHAR" (:tag (first (:subordinate-lines (first records))))))
-      (is (= "SUBM" (:tag (second records))))
-      (is (= "TRLR" (:tag (first (reverse records)))))
+    (let [root (read-file (clojure.java.io/resource "simple.ged"))]
+      (is (= "__ROOT__" (:tag root)))
+      (let [records (:subordinate-lines root)]
+        (is (= 7 (count records)))
+        (is (= "HEAD" (:tag (first records))))
+        (is (= "CHAR" (:tag (first (:subordinate-lines (first records))))))
+        (is (= "SUBM" (:tag (second records))))
+        (is (= "TRLR" (:tag (first (reverse records))))))
       ;; (is (= "Submitters address\naddress continued here"
       ;;        (get-in [:subordinate-lines :tag] (second records))))
       ))
 
   (testing "the big file"
-    (let [records (read-file (clojure.java.io/resource "allged.ged"))]
-      (is (= 18 (count records)))))
+    (let [root (read-file (clojure.java.io/resource "allged.ged"))]
+      (is (= 18 (count (:subordinate-lines root))))))
 
   (testing "error conditions"
-    (is (nil? (read-file "does-not-exist.ged")))))
+    (is (= (read-file "does-not-exist.ged")
+           {:tag "__ROOT__" :level -1}))))
--- test/ahnentafel/gedcom/reading/semantic_test.clj
+++ test/ahnentafel/gedcom/reading/semantic_test.clj
@@ -4,46 +4,52 @@
 
 (deftest grouping-records
   (is (= (process-records '({:level 0 :tag "HEAD"}
-                          {:level 0 :tag "INDI"}))
-         '[{:level 0 :tag "HEAD"}
-           {:level 0 :tag "INDI"}]))
-
-  (is (= (process-records '({:level 0 :tag "HEAD"} {:level 1 :tag "CHAR"}))
-         [{:level 0
-           :tag "HEAD"
-           :subordinate-lines [{:level 1 :tag "CHAR"}]}]))
+                            {:level 0 :tag "INDI"}))
+         {:level -1 :tag "__ROOT__"
+          :subordinate-lines[{:level 0 :tag "HEAD"}
+                             {:level 0 :tag "INDI"}]}))
 
   (is (= (process-records '({:level 0 :tag "HEAD"}
-                          {:level 1 :tag "GEDC"}
-                          {:level 2 :tag "VERS"}))
-         [{:level 0
-           :tag "HEAD"
-           :subordinate-lines [{:level 1
-                                :tag "GEDC"
-                                :subordinate-lines [{:level 2
-                                                     :tag "VERS"}]}]}]))
+                            {:level 1 :tag "CHAR"}))
+         {:level -1 :tag "__ROOT__"
+          :subordinate-lines [{:level 0
+                               :tag "HEAD"
+                               :subordinate-lines [{:level 1 :tag "CHAR"}]}]}))
 
   (is (= (process-records '({:level 0 :tag "HEAD"}
-                          {:level 1 :tag "CHAR"}
-                          {:level 0 :tag "INDI"}))
-         [{:level 0
-           :tag "HEAD"
-           :subordinate-lines [{:level 1 :tag "CHAR"}]}
-          {:level 0
-           :tag "INDI"}]))
+                    {:level 1 :tag "GEDC"}
+                    {:level 2 :tag "VERS"}))
+         {:level -1 :tag "__ROOT__"
+          :subordinate-lines [{:level 0
+                               :tag "HEAD"
+                               :subordinate-lines [{:level 1
+                                                    :tag "GEDC"
+                                                    :subordinate-lines [{:level 2
+                                                                         :tag "VERS"}]}]}]}))
 
   (is (= (process-records '({:level 0 :tag "HEAD"}
-                          {:level 1 :tag "CHAR"}
-                          {:level 0 :tag "INDI"}
-                          {:level 1 :tag "NAME"}))
-         [{:level 0
-           :tag "HEAD"
-           :subordinate-lines [{:level 1 :tag "CHAR"}]}
-          {:level 0
-           :tag "INDI"
-           :subordinate-lines [{:level 1 :tag "NAME"}]}]))
+                    {:level 1 :tag "CHAR"}
+                    {:level 0 :tag "INDI"}))
+         {:level -1 :tag "__ROOT__"
+          :subordinate-lines [{:level 0
+                               :tag "HEAD"
+                               :subordinate-lines [{:level 1 :tag "CHAR"}]}
+                              {:level 0 :tag "INDI"}]}))
+
+  (is (= (process-records '({:level 0 :tag "HEAD"}
+                    {:level 1 :tag "CHAR"}
+                    {:level 0 :tag "INDI"}
+                    {:level 1 :tag "NAME"}))
+         {:level -1 :tag "__ROOT__"
+          :subordinate-lines [{:level 0
+                               :tag "HEAD"
+                               :subordinate-lines [{:level 1 :tag "CHAR"}]}
+                              {:level 0
+                               :tag "INDI"
+                               :subordinate-lines [{:level 1 :tag "NAME"}]}]}))
 
   ;; (is (= (process-records '({:level 0 :tag "STUFF" :value "Bob"}
   ;;                           {:level 1 :tag "CONT" :value "more Bob"}))
-  ;;        [{:level 0 :tag "STUFF" :value "Bob\nmore Bob"}]))
+  ;;        {:level -1 :tag "__ROOT__"
+  ;;         :subordinate-lines [{:level 0 :tag "STUFF" :value "Bob\nmore Bob"}]}))
   )
