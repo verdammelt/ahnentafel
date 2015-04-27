c793f07191382441536397aebeaaf84d80318acf
Mark Simpson <verdammelt@gmail.com>
2015-04-27 11:38:59 -0400

    Filling out more header data.
    NOTE: submitter not working correctly yet.

--- src/ahnentafel/server/pages.clj
+++ src/ahnentafel/server/pages.clj
@@ -1,12 +1,23 @@
 (ns ahnentafel.server.pages
-  (:require [net.cgrand.enlive-html :as html]))
+  (:require [net.cgrand.enlive-html :as html])
+  (:require [ahnentafel.gedcom.data :as data]))
 
 (html/defsnippet home-page-snippet "site/templates/home.html" [:div] [data]
-  (let [file (:file data)
-        num-records (count ((:get-data data)))]
+  (let [tree ((:get-data data))
+        header (data/header tree)]
     [:#home-contents]
     (html/transform-content
-     (html/replace-vars {:num-records (str num-records) :file (str file)}))))
+     (html/replace-vars {:number-of-records (str (:number-of-records header))
+                         :file (:file header)
+                         :source-name (:source header)
+                         :destination-name (:destination header)
+                         :file-time (:file-time header)
+                         :gedcom-version (get-in header [:gedcom :version])
+                         :gedcom-type (get-in header [:gedcom :type])
+                         :encoding (:encoding header)
+                         :submitter (:submitter header)}))
+
+    ))
 
 
 (html/deftemplate home-page "site/templates/index.html" [data]
