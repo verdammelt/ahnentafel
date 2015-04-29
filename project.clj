132690210c6236c03043f880541cfb15bb0ce51d
Mark Simpson <verdammelt@gmail.com>
2015-04-29 16:45:59 -0400

    Setting up Project for Heroku Deployment.
    * Adding min-lein-version to project.clj
    * Adding "resources/data" to resources-path
    * Adding prep-task to compile ParserError in production (why isn't
      compile on Heroku doing the AOT build?)
    * Procfile to run 'ring server-headless' in production.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.3"]
                  [ring "1.3.2"]
@@ -17,8 +18,13 @@
   :ring {:init ahnentafel.system/ring-init
          :handler ahnentafel.system/the-handler}
 
+  :resource-paths ["resources" "resources/data"]
+
   :profiles
-  {:dev
+  {:production
+   {:prep-tasks [["compile" "ahnentafel.gedcom.reading.ParseError"]]}
+
+   :dev
    {:source-paths ["dev"]
     :resource-paths ["test-resources"]
     :plugins [[lein-ancient "0.6.7"]
