2fdf05333df1411ed60b81026687c1aaa3680be1
Mark Simpson <verdammelt@gmail.com>
2015-04-30 10:55:14 -0400

    Extracted helper for getting the pages in the test.

--- test/ahnentafel/server/handler_test.clj
+++ test/ahnentafel/server/handler_test.clj
@@ -7,25 +7,26 @@
 
 (deftest handlers
   (let [app-data {:version "x.x.x"}
-        handler (make-handler app-data)]
+        handler (make-handler app-data)
+        get-page (fn [url] (handler (mock/request :get url)))]
+
    (testing "home page"
      (with-redefs [pages/home (fn [data] (str "homepage: " data))]
-       (let [response (handler (mock/request :get "/"))]
+       (let [response (get-page "/")]
          (is (= (:status response) 200))
          (is (= (:body response)
                 (str "homepage: " app-data))))))
 
    (testing "404 response"
-     (let [response (handler (mock/request :get "/unknown"))]
+     (let [response (get-page "/unknown")]
        (is (= (:status response) 404))
        (is (.contains (:body response) "/unknown not found."))
-       (is (= (get-in response [:headers  "Content-Type"])
-              "text/html"))))
+       (is (= (get-in response [:headers  "Content-Type"]) "text/html"))))
 
    (testing "record page"
      (with-redefs [pages/record (fn [data & xref]
                                   (str "record page: " data " " xref))]
-       (let [response (handler (mock/request :get "/records/@I23@"))]
+       (let [response (get-page "/records/@I23@")]
          (is (= (:status response) 200))
          (is  (= (:body response)
                  (str "record page: " app-data " " '("@I23@")))))))))
