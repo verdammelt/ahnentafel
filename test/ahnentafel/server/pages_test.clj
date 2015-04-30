b19595839bcabf367610db57d71b15acb4c6634d
Mark Simpson <verdammelt@gmail.com>
2015-04-30 11:52:40 -0400

    Cleaning up tests - pulling out helpers.

--- test/ahnentafel/server/pages_test.clj
+++ test/ahnentafel/server/pages_test.clj
@@ -3,6 +3,30 @@
   (:require [ahnentafel.gedcom.data :as data])
   (:require [clojure.test :refer :all]))
 
+(defn get-page [page & args]
+  (apply str (apply page
+                    {:version "x.x.x"
+                    :gedcom-file "/path/to/test.ged"
+                     :get-data (fn [] 'fake-data)}
+                    args)))
+
+(defmethod assert-expr 'page-contains? [msg form]
+  (let [text (nth form 1)
+        msg (or msg (str "Expected to find '" text "' on the page."))]
+    `(let [result# (.contains ~'page ~text)]
+       (do-report {:message ~msg :expected '~form :actual ~'page
+                   :type (if result# :pass :fail)}))))
+
+(defmethod assert-expr 'page-not-contains? [msg form]
+  (let [text (nth form 1)
+        msg (or msg (str "Expected not to find '" text "' on the page."))]
+    `(let [result# (.contains ~'page ~text)]
+       (do-report {:message ~msg :expected '~form :actual ~'page
+                   :type (if result# :fail :pass)}))))
+
+(defmacro are-on-page [& texts]
+  `(are [text] (~'page-contains? text) ~@texts))
+
 (deftest home-page-test
   (with-redefs [data/header (constantly {:number-of-records 23
                                          :source "Author"
@@ -14,24 +38,24 @@
                                          :encoding "ANSEL"
                                          :submitter {:xref "@I31@"
                                                      :name "Fred Smith"}})]
-    (let [page (apply str (home {:version "x.x.x"
-                                      :get-data (fn [] 'fake-data)
-                                      :file "/path/to/test.ged"}))]
-      (is (.contains page "<title>Ahnentafel</title>"))
-      (is (.contains page "23 records in test.ged."))
-      (is (.contains page "id=\"home-contents\""))
-      (is (.contains page "created by Author"))
-      (is (.contains page "for Recipient"))
-      (is (.contains page "on 13 SEP 2000"))
-      (is (.contains page "GEDCOM version 5.5.1 (LINEAGE-LINKED ANSEL)"))
-      (is (.contains page "Submitted by <a href=\"/records/@I31@\">Fred Smith</a>"))
-      (is (.contains page "<span id=\"version\">x.x.x</span>")))))
+    (let [page (get-page home)]
+      (are-on-page
+       "<title>Ahnentafel</title>"
+       "23 records in test.ged."
+       "id=\"home-contents\""
+       "created by Author"
+       "for Recipient"
+       "on 13 SEP 2000"
+       "GEDCOM version 5.5.1 (LINEAGE-LINKED ANSEL)"
+       "Submitted by <a href=\"/records/@I31@\">Fred Smith</a>"
+       "<span id=\"version\">x.x.x</span>"))))
 
 (deftest page-not-found-test
-  (let [page (apply str (not-found {:version "x.x.x"} {:uri "/unknown"}))]
-    (is (.contains page "<title>Ahnentafel</title>"))
-    (is (.contains page "/unknown not found."))
-    (is (.contains page "<span id=\"version\">x.x.x</span>"))))
+  (let [page (get-page not-found {:uri "/unknown"})]
+    (are-on-page
+     "<title>Ahnentafel</title>"
+     "/unknown not found."
+     "<span id=\"version\">x.x.x</span>")))
 
 (deftest record-page-test
   (testing "individual record - maximum data"
@@ -51,21 +75,21 @@
                        :family-as-child "@FAM1@"
                        :family-as-spouse "@FAM2@"
                        })]
-        (let [page (apply str (record {:get-data (fn [] 'fake-data)} "@I23@"))]
+        (let [page (get-page record "@I23@")]
           (is (= @trapped-query {:xref "@I23@"}))
-          (is (.contains page "INDIVIDUAL"))
-          (is (.contains page "Bob Smith (a.k.a. Robert Smith, The Guy from The Cure)"))
-          (is (.contains page "Sex: M"))
-          (is (.contains page "Born: 1 JAN 1970 00:00:00 near his mother"))
-          (is (.contains page "Died: 1 JAN 2000 00:00:00 graveside"))
-          (is (.contains page "Buried: 2 JAN 2000 00:00:00 6 feet under"))
-          (is (.contains page "<a href=\"/records/@FAM1@\">Go To Family (where this person was a child)</a>"))
-          (is (.contains page "<a href=\"/records/@FAM2@\">Go To Family (where this person was a spouse)</a>") page)))))
+          (are-on-page
+           "INDIVIDUAL"
+           "Bob Smith (a.k.a. Robert Smith, The Guy from The Cure)"
+           "Sex: M"
+           "Born: 1 JAN 1970 00:00:00 near his mother"
+           "Died: 1 JAN 2000 00:00:00 graveside"
+           "Buried: 2 JAN 2000 00:00:00 6 feet under"
+           "<a href=\"/records/@FAM1@\">Go To Family (where this person was a child)</a>"
+           "<a href=\"/records/@FAM2@\">Go To Family (where this person was a spouse)</a>")))))
 
   (testing "individual record - with parts missing"
     (with-redefs [data/find-record
                   (constantly {:type :individual})]
-      (let [page (apply str (record {:xref "@I23@" :get-data (constantly 'fake-data)}))]
-        (doseq [x '("Name" "Sex" "Born" "Died" "Buried" "Go To Family")]
-          (is (not (.contains page x))
-              (str "Should not find '" x "' on page.")))))))
+      (let [page (get-page record "@I23@")]
+        (are [text] (page-not-contains? text)
+             "Name" "Sex" "Born" "Died" "Buried" "Go To Family")))))
