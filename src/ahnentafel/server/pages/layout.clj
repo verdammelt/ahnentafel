89f10f4cb16917b764bd6433cae28c229eaef99d
Mark Simpson <verdammelt@gmail.com>
2015-05-21 21:55:36 -0400

    Docstring for posterity.

--- src/ahnentafel/server/pages/layout.clj
+++ src/ahnentafel/server/pages/layout.clj
@@ -3,6 +3,9 @@
 
 
 (defmacro def-layout-template [name & forms]
+  "Anaphoric macro for defining templates in the 'standard' layout.
+Defines two parmaters: DATA (the data passed down from the handler) and
+ARGS (rest argument)."
   `(html/deftemplate ~name "site/templates/index.html" [~'data & ~'args]
      ~@forms
      [:#version] (html/content (:version ~'data))))
