ea5f37c3c1002d4f1836dc9225fbfbac0e8ead15
Mark Simpson <verdammelt@gmail.com>
2015-07-22 07:14:30 -0400

    Fixing project URL.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ahnentafel "0.1.0-SNAPSHOT"
   :description "Application to allow browsing of genealogical data."
-  :url "http://example.com/FIXME"
+  :url "http://ahnentafel.herokuapps.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
